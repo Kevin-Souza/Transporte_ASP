96fd1c502325a267bad8a0cc805ff5a4ab4c7cad
Kevin_Souza <kevinsouza1.ks@gmail.com>
2022-05-04 17:09:11 -0300

    add model veiculo

---
+++ Controle_Frota/Models/Marcas.cs
@@ -0,0 +1,10 @@
+﻿namespace Controle_Frota.Models
+{
+    public class Marcas
+    {
+        public Guid MarcaId { get; set; }
+        public ICollection<Veiculo> veiculos { get; set; }
+    }
+
+
+}
---
+++ Controle_Frota/Models/Veiculo.cs
@@ -0,0 +1,15 @@
+﻿namespace Controle_Frota.Models
+{
+    public class Veiculo
+    {
+        public Guid Id { get; set; }
+        public string Modelo { get; set; }
+        public string Placa { get; set; }
+        public int Ano { get; set; }
+        public string MarcaId { get; set; }
+        public Marcas Marca { get; set; }
+        public string Oficina { get; set; }
+        //public Setores Setor  { get; set; }
+        public string Tipo { get; set; }
+    }
+}
